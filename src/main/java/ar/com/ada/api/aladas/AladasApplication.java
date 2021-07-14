2aa04c6bccc9b5717a0354545a007e66e1ce21ab
Pzacarias <pzacarias.4@gmail.com>
2021-07-14 19:25:59 -0300

    primer commit

---
+++ src/main/java/ar/com/ada/api/aladas/AladasApplication.java
@@ -0,0 +1,13 @@
+package ar.com.ada.api.aladas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AladasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AladasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/com/ada/api/aladas/AladasApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.ada.api.aladas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AladasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
