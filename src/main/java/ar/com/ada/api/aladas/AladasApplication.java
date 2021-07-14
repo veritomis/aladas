a8a691f918b99b892998d0a621153fc46b53799f
Pzacarias <pzacarias.4@gmail.com>
2021-07-14 19:31:20 -0300

    comentario sobre la aplicacion

--- src/main/java/ar/com/ada/api/aladas/AladasApplication.java
+++ src/main/java/ar/com/ada/api/aladas/AladasApplication.java
@@ -3,6 +3,8 @@ package ar.com.ada.api.aladas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Aplicacion de sistema para una aerolinea
+
 @SpringBootApplication
 public class AladasApplication {
 
