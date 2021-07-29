6bb088290a7e515e62833cafbc1f4fd3a00074e5
ShadowDrone <shadowdrone@hotmail.com>
2021-07-28 21:43:13 -0300

    Se crean un par de UNIT Testing a desarrollar pensando en la funcionalidad que tendriamos que testear. En el caso de codigos IATA hicimos un meotod que nos permite validar el formato del CODIGO IATA para que sea solo LETRAS en mayuscula.
    Co-authored-by: BrendaMonteagudo1 <BrendaMonteagudo1@users.noreply.github.com>
    Co-authored-by: sami1793 <sami1793@users.noreply.github.com>
    Co-authored-by: Melwil1989 <Melwil1989@users.noreply.github.com>
    Co-authored-by: Nurys Ochoa <NurysOchoa@users.noreply.github.com>
    Co-authored-by: tpcolque <tpcolque@users.noreply.github.com>
    Co-authored-by: flordife <flordife@users.noreply.github.com>
    Co-authored-by: monpn <monpn@users.noreply.github.com>
    Co-authored-by: byteMe907 <byteMe907@users.noreply.github.com>
    Co-authored-by: MaríaEugenia <mariaeugeniachaparro@users.noreply.github.com>
    Co-authored-by: Muriel3000 <Muriel3000@users.noreply.github.com>
    Co-authored-by: Pzacarias <Pzacarias@users.noreply.github.com>
    Co-authored-by: AgosBC <AgosBC@users.noreply.github.com>
    Co-authored-by: YracemaM <YracemaM@users.noreply.github.com>
    Co-authored-by: Sabrim2401 <Sabrim2401@users.noreply.github.com>
    Co-authored-by: viviflaquita <viviflaquita@users.noreply.github.com>
    Co-authored-by: carolinaportillo <carolinaportillo@users.noreply.github.com>
    Co-authored-by: Bluemavim <Bluemavim@users.noreply.github.com>
    Co-authored-by: magira01 <magira01@users.noreply.github.com>
    Co-authored-by: Linacastellanos95 <Linacastellanos95@users.noreply.github.com>
    Co-authored-by: Miliiplx <Miliiplx@users.noreply.github.com>

--- src/main/java/ar/com/ada/api/aladas/services/AeropuertoService.java
+++ src/main/java/ar/com/ada/api/aladas/services/AeropuertoService.java
@@ -32,7 +32,28 @@ public class AeropuertoService {
 
     }
 
-    public Aeropuerto buscarPorCodigoIATA(String codigoIATA){
+    public Aeropuerto buscarPorCodigoIATA(String codigoIATA) {
         return repo.findByCodigoIATA(codigoIATA);
+    }
+
+    public boolean validarCodigoIATA(Aeropuerto aeropuerto) {
+
+        // SI viene algo diferente de 3, que salga.
+        if (aeropuerto.getCodigoIATA().length() != 3)
+            return false;
+
+        String codigoIATA = aeropuerto.getCodigoIATA();
+
+        // " AP"
+        for (int i = 0; i < codigoIATA.length(); i++) {
+            char c = codigoIATA.charAt(i);
+
+            if (!(c >= 'A' && c <= 'Z'))
+                return false;
+
+        }
+
+        return true;
+
     }
 }
--- src/test/java/ar/com/ada/api/aladas/AladasApplicationTests.java
+++ src/test/java/ar/com/ada/api/aladas/AladasApplicationTests.java
@@ -7,8 +7,11 @@ import java.math.BigDecimal;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestExecutionListeners;
 
+import ar.com.ada.api.aladas.entities.Aeropuerto;
 import ar.com.ada.api.aladas.entities.Vuelo;
+import ar.com.ada.api.aladas.services.AeropuertoService;
 import ar.com.ada.api.aladas.services.VueloService;
 
 @SpringBootTest
@@ -16,6 +19,9 @@ class AladasApplicationTests {
 
 	@Autowired
 	VueloService vueloService;
+
+	@Autowired
+	AeropuertoService aeropuertoService;
 
 	@Test
 	void vueloTestPrecioNegativo() {
@@ -38,6 +44,89 @@ class AladasApplicationTests {
 		// Assert: afirmar
 		// afirmar quie sea verdadero: assertTrue
 		assertTrue(vueloService.validarPrecio(vueloConPrecioOK));
+
+	}
+
+	@Test
+	void aeropuertoValidarCodigoIATAOK() {
+		// From Florencia Di Felice to Everyone: 07:42 PM
+		// el código no debe llevar número y sólo 3 letras, así que habría que limitarlo
+		// a eso, no?
+
+		String codigoIATAOk1 = "EZE";
+		String codigoIATAOk2 = "AEP";
+		String codigoIATAOk3 = "NQN";
+		String codigoIATAOk4 = "N  ";
+		String codigoIATAOk5 = "N39";
+
+		/*//String codigoIATAOk4 = "N  ";
+
+		//En este caso, afirmo que espero que el length del codigoIATAOk1 sea 3
+		assertEquals(3, codigoIATAOk1.length());
+
+		//En este caso, afirmo que espero qeu el resultado de la condicion
+		//sea verdaderro(en este caso, lenght == 3)
+		assertTrue(codigoIATAOk2.length() == 3);
+
+		//assertTrue(codigoIATAOk4.length() == 3);*/
+
+		Aeropuerto aeropuerto1 = new Aeropuerto();
+		aeropuerto1.setCodigoIATA(codigoIATAOk1);
+
+		Aeropuerto aeropuerto2 = new Aeropuerto();
+		aeropuerto2.setCodigoIATA(codigoIATAOk2);
+
+		Aeropuerto aeropuerto3= new Aeropuerto();
+		aeropuerto3.setCodigoIATA(codigoIATAOk3);
+
+		
+		Aeropuerto aeropuerto4= new Aeropuerto();
+		aeropuerto4.setCodigoIATA(codigoIATAOk4);
+
+		assertTrue(aeropuertoService.validarCodigoIATA(aeropuerto1));
+		assertTrue(aeropuertoService.validarCodigoIATA(aeropuerto2));
+		assertTrue(aeropuertoService.validarCodigoIATA(aeropuerto3));
+
+
+		assertFalse(aeropuertoService.validarCodigoIATA(aeropuerto4));
+
+		
+	}
+
+	@Test
+	void aeropuertoValidarCodigoIATANoOK() {
+		// From Florencia Di Felice to Everyone: 07:42 PM
+		// el código no debe llevar número y sólo 3 letras, así que habría que limitarlo
+		// a eso, no?
+
+	}
+
+	@Test
+	void vueloVerificarValidacionAeropuertoOrigenDestino() {
+		// En este validar todas las posibilidades de si el aeropuerto
+		// origen es igual al de destion o todo lo que se les ocurra
+	}
+
+	@Test
+	void vueloChequearQueLosPendientesNoTenganVuelosViejos() {
+
+		// Queremos validar que cuando hagamos un metodo que traiga los vuelos actuales
+		// para
+		// hacer reservas, no haya ningun vuelo en el pasado.
+	}
+
+	@Test
+	void vueloVerificarCapacidadMinima() {
+
+	}
+
+	@Test
+	void vueloVerificarCapadidadMaxima() {
+
+	}
+
+	@Test
+	void aeropuertoTestBuscadorIATA() {
 
 	}
 
